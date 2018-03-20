a29a75ffa9f1d67fd258815ae7d3d39edd8cd27d
shangzhixin <shangzhixin@sumaott.com>
2018-03-20 14:47:12 +0800

    修改getAppInfo.sh

--- getAppInfo.sh
+++ getAppInfo.sh
@@ -13,9 +13,10 @@ bundle_identifier=`/usr/libexec/PlistBuddy -c "Print CFBundleIdentifier" $info_p
 app_version=`/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" $info_plist_path`
 commit_Id=`git rev-parse --short HEAD`
 commit_version=`git rev-list head | sort | wc -l | awk '{print $1}'`
+project_branch=`git symbolic-ref --short -q HEAD`
 
 echo "COMMITID=${commitId:0:6}" > commitId.txt 
 echo "APP_NAME=${app_Name}" > appName.txt
 echo "APP_VERSION=${app_version}(${commit_version})" > appVersion.txt
-
+echo "CUR_BRANCH=${project_branch}" > curBranch.txt
 
