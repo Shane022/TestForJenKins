4219653a6c3522be0fc3ceda4d8c4a9a65cd65e7
shangzhixin <shangzhixin@sumaott.com>
2018-03-20 14:55:43 +0800

    修改脚本

--- getAppInfo.sh
+++ getAppInfo.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 project_name=$1
 info_plist_name=Info
-info_plist_path="$project_name/$info_plist_name.plist"
+info_plist_path="${project_name}/${info_plist_name}.plist"
+
+echo $info_plist_path
 
 # 获取commitId
 # 同理获取子模块的commitId
 # cd TestForJenkins/
 # echo $(pwd)
 
-app_Name=`/usr/libexec/PlistBuddy -c "Print CFBundleDisplayName" $info_plist_path`
-bundle_identifier=`/usr/libexec/PlistBuddy -c "Print CFBundleIdentifier" $info_plist_path`
-app_version=`/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" $info_plist_path`
+app_Name=`/usr/libexec/PlistBuddy -c "Print CFBundleDisplayName" ${info_plist_path}`
+# bundle_identifier=`/usr/libexec/PlistBuddy -c "Print CFBundleIdentifier" $info_plist_path`
+app_version=`/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" ${info_plist_path}`
 commit_Id=`git rev-parse --short HEAD`
 commit_version=`git rev-list head | sort | wc -l | awk '{print $1}'`
 project_branch=`git symbolic-ref --short -q HEAD`
 
-echo "COMMITID=${commitId:0:6}" > commitId.txt 
+echo "COMMITID=${commit_Id:0:6}" > commitId.txt 
 echo "APP_NAME=${app_Name}" > appName.txt
 echo "APP_VERSION=${app_version}(${commit_version})" > appVersion.txt
 echo "CUR_BRANCH=${project_branch}" > curBranch.txt
