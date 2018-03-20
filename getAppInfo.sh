9c82c5ae53dd6922660f66f96f9d9929ffcef281
shangzhixin <shangzhixin@sumaott.com>
2018-03-20 14:22:27 +0800

    修改项目名称

--- getAppInfo.sh
+++ getAppInfo.sh
@@ -1,8 +1,21 @@
-#获取commitId
+#!/bin/bash
+project_name=$1
+info_plist_name=Info
+info_plist_path="$project_name/$info_plist_name.plist"
+
+# 获取commitId
 # 同理获取子模块的commitId
 # cd TestForJenkins/
 # echo $(pwd)
-commitId=`git rev-parse --short HEAD`
-echo "COMMITID=${commitId:0:6}" > commitId.properties 
+
+app_Name=`/usr/libexec/PlistBuddy -c "Print CFBundleDisplayName" $info_plist_path`
+bundle_identifier=`/usr/libexec/PlistBuddy -c "Print CFBundleIdentifier" $info_plist_path`
+app_version=`/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" $info_plist_path`
+commit_Id=`git rev-parse --short HEAD`
+commit_version=`git rev-list head | sort | wc -l | awk '{print $1}'`
+
+echo "COMMITID=${commitId:0:6}" > commitId.txt 
+echo "APP_NAME=${app_Name}" > appName.txt
+echo "APP_VERSION=${app_version}(${commit_version})" > appVersion.txt
 
 
