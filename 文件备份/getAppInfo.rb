ef357bc9614897de5d54e90d34cbfa66c775eecf
shangzhixin <shangzhixin@sumaott.com>
2018-03-27 16:51:30 +0800

    完善脚本

--- 文件备份/getAppInfo.rb
+++ 文件备份/getAppInfo.rb
@@ -1,13 +1,14 @@
 #!/usr/bin/ruby
 require 'FileUtils'
 
-
-
+# ******************************************
+# 参数说明 
 # $*[0] scheme名称
 # $*[1]	start tag
 # $*[2] end tag
 
 # ******************************************
+# 基础信息获取
 project_name=$*[0]
 info_plist_name="Info"
 info_plist_path="#{project_name}/#{info_plist_name}.plist"
@@ -23,44 +24,50 @@ git_changelog=`git log --pretty=format:"%s" #{$*[1]}...#{$*[2]}`
 # 创建Jenkins环境变量文件夹
 FileUtils.mkpath 'jenkins_properties'
 dirName = "jenkins_properties"
+
 # ******************************************
-# Git log 
+
+# Git log ----------------------------------
+# 摘出bugId
+arr = git_changelog.scan /[^#]+/m   
+finalGitLog = arr.join(",")
 file = File.open("#{dirName}/gitlog.txt","w+")
-file.syswrite("#{git_changelog}")
+file.syswrite("#{finalGitLog}")
 file.close
 # 将changelog的换行符替换为<br>标签
 changelog = `cat #{dirName}/gitlog.txt | awk '{printf "%s<br>",$1}'`
 file = File.open("#{dirName}/gitlog.txt","w+")
 file.syswrite("CHANGELOG=#{changelog}")
 file.close
-# Commit Id
+
+# Commit Id ----------------------------------
 commitId = "#{commit_Id}"[0,6]
 file = File.open("#{dirName}/commitId.txt","w+")
 file.syswrite("COMMITID=#{commitId}")
 file.close
-# App Name
+
+# App Name ----------------------------------
 file = File.open("#{dirName}/appName.txt","w+")
 file.syswrite("APP_NAME=#{app_Name}")
 file.close
-# App Version
+
+# App Version ----------------------------------
 file = File.open("#{dirName}/appVersion.txt","w+")
 file.syswrite("APPVERSION=#{app_version}")
 file.close
-# Commit Version
+
+# Commit Version ----------------------------------
 file = File.open("#{dirName}/commitVersion.txt","w+")
 file.syswrite("COMMITVERSION=#{commit_version}")
 file.close
-# Branch
+
+# Branch ----------------------------------
 file = File.open("#{dirName}/curBranch.txt","w+")
 file.syswrite("CUR_BRANCH=#{project_branch}")
 file.close
 
-# arr = IO.read("#{dirName}/gitlog.txt")
-# # final = /^#([^#]+)/m.match(arr)
-# final = arr.scan /[^#]+/m   
-# puts final
+# ******************************************
 
-# filterChangelog = /#(.+)/.match("#{changelog}")
 
 
 
