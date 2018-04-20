2636fe7b8a6c573680cfaec5352998a4a09ba3ed
shangzhixin <shangzhixin@sumaott.com>
2018-04-20 17:07:49 +0800

    修改构建脚本

---
+++ test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+workspace=$(pwd)
+# 每次构建之前清楚上次的构建结果
+# ---------------------------
+rm -rf $workspace/ipa
+# fastlane 构建
+# ---------------------------
+fastlane test workspace:$workspace
+# fir上传
+# ---------------------------
+ipaPath="$workspace/ipa/"
+cd $ipaPath
+
+files=`ls $ipaPath`
+ipaName=''
+for file in $files; do
+	if [[ $file =~ "ipa" ]]; then
+		ipaName=$file
+	fi
+done
+
+fir --no-open --qrcode --password=123456 publish $ipaName
